9fc7ef3809d18f69e00a55c5ab7c7098fe97b85f
vearne <asdwoshiaotian@gmail.com>
2021-03-16 17:29:08 +0800

    add time window limiter in memory

---
+++ example/time_window_alg.go
@@ -0,0 +1,45 @@
+package main
+
+import (
+	"fmt"
+	"github.com/vearne/ratelimit"
+	"math/rand"
+	"sync"
+	"time"
+)
+
+
+func consume(r ratelimit.Limiter, group *sync.WaitGroup,
+	c * ratelimit.Counter, targetCount int) {
+	group.Add(1)
+	defer group.Done()
+	for {
+		ok, err := r.Take()
+		if err != nil {
+			ok = true
+			fmt.Println("error", err)
+		}
+		if ok {
+			value := c.Incre()
+			if value >= targetCount{
+				break
+			}
+		} else {
+			time.Sleep(time.Duration(rand.Intn(10)+1) * time.Millisecond)
+		}
+	}
+}
+func main() {
+	limiter := ratelimit.NewSlideTimeWindowRatelimit(100, time.Second, 100)
+
+	var wg sync.WaitGroup
+	total := 500
+	counter := ratelimit.NewCounter()
+	start := time.Now()
+	for i := 0; i < 100; i++ {
+		go consume(limiter, &wg, counter, total)
+	}
+	wg.Wait()
+	cost := time.Since(start)
+	fmt.Println("cost", time.Since(start), "rate", float64(total)/cost.Seconds())
+}
